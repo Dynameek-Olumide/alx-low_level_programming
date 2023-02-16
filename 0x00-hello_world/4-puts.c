f7c84d71adb8c440ab2077cbed8023012a171319
Dynameek-Olumide <adedayur@gmail.com>
2023-02-16 15:19:46 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash#include <stdio.h>
+/**
+ * main - A program that print a line
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
