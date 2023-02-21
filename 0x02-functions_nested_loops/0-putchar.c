841ffd26e8ea7634e62743448fdbd7702d22d246
Dynameek-Olumide <adedayur@gmail.com>
2023-02-21 14:58:02 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,22 @@
-#include <unistd.h>
+#include "main.h"
+	/**
+	 * main - print _putchar
+	 * is written by olumide
+	 * Return: 0
+	 */
 
-int main() {
-    char message[] = "_putchar\n";
-    write(1, message, sizeof(message)-1);
-    return 0;
+
+	int main(void)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+	}
 }
